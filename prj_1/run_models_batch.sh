ad8c899d27a72365de3f3e1f7209b1a96b0ddfff
Tang, Bin <77304+tang-bin@users.noreply.github.com>
2020-05-10 18:15:33 -0700

    add result and images to prj 1

--- prj_1/run_models_batch.sh
+++ prj_1/run_models_batch.sh
@@ -9,12 +9,12 @@
 #
 # Usage: sh run_models_batch.sh    -- will run program from commandline within Project Workspace
 #  
-# python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
-# python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
-# python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
+python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
 
-python check_images.py --arch resnet > rst_all.txt
-echo "--------------------" >> rst_all.txt
-python check_images.py --arch alexnet >> rst_all.txt
-echo "--------------------" >> rst_all.txt
-python check_images.py --arch vgg >> rst_all.txt
+# python check_images.py --arch resnet > rst_all.txt
+# echo "--------------------" >> rst_all.txt
+# python check_images.py --arch alexnet >> rst_all.txt
+# echo "--------------------" >> rst_all.txt
+# python check_images.py --arch vgg >> rst_all.txt
